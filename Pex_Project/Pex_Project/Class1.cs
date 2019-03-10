611cf7df51ee5d3a979c9ec5e363f55291e4a0de
Vijay1712 <vjagtap2@illinois.edu>
2019-03-10 13:28:54 -0500

    upload code for Pex Runner

---
+++ Pex_Project/Pex_Project/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Pex_Project
+{
+    public class Class1
+    {
+    }
+}
---
+++ Pex_Project/Pex_Project/PexRunner.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Pex_Project
+{
+    public class Command
+    {
+        public string executable;
+        public string arguments;
+        public Command()
+        {
+        }
+        public Command(string executable, string arguments)
+        {
+            this.executable = executable;
+            this.arguments = arguments;
+        }
+    }
+
+    public class PexRunner
+    {
+
+        static void Main(string[] args)
+        {
+            Command temp = Run_Pex_Command();
+            ExecuteCommand(temp);
+        }
+
+
+        public static Command Run_Pex_Command()
+        {
+            Command command = new Command();
+            command.executable = @"C:\Program Files\Microsoft Pex\bin\pex.x86.exe";
+            return command;
+        }
+
+        public static void ExecuteCommand(Command command)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = false;
+            startInfo.UseShellExecute = false;
+            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            startInfo.FileName = command.executable;
+            startInfo.Arguments = command.arguments;
+
+            Console.WriteLine(command.executable);
+            Console.WriteLine(command.arguments);
+
+            try
+            {
+                // Start the process with the info we specified.
+                // Call WaitForExit and then the using statement will close.
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+
+                    exeProcess.WaitForExit();
+                }
+                //Console.WriteLine("finished");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+    }
+}
